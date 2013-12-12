b22360b92f0b0f7b778bcc7d10f70a34abf91dbf
Michael Finneran <mrfinneran@gmail.com>
2013-12-12 12:11:49 -0500

    added name to user table

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Conectup::Application.routes.draw do
  
   devise_for :users
-  root :to => "home#index"
+  root :to => "users#show"
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
