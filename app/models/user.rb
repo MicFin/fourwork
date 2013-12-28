ebf663bff4f74f09c55e3221950b67bec54cc046
Michael Finneran <mrfinneran@gmail.com>
2013-12-28 15:16:58 -0500

    pushing to heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,16 @@ class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true
   
+  def friends_of_friend(the_friend)
+    friends_of_friend = []
+    self.friends.each do |friend|
+      the_friend.friends.each do |friends_friend|
+        if friend == friends_friend
+          friends_of_friend << friend 
+        end
+      end
+    end
+    return friends_of_friend
+  end
+
 end
